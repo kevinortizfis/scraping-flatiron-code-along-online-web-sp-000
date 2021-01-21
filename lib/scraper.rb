50ba6b5b23bbe2df621efaae79a5b550ae4e044a
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2021-01-21 00:20:39 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,6 +18,7 @@ class Scraper
 
   def get_page
     html = open("https://flatironschool.com/")
+    doc = Nokogiri::HTML(open("https://twitter.com/home"))
     page = Nokogiri::html
     page
   end
